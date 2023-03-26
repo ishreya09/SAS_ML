fc99abc634e40966c94683f552723b76fa20c008
Shreya Reddy <shreya.tippireddy@gmail.com>
2023-03-26 08:09:25 +0530

    fn

--- frontend.py
+++ frontend.py
@@ -1,53 +1,56 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from PIL import Image
 import datetime
 import os
+import tensorflow as tf
+from predict import model_prediction,set_data
+from damage import damage_percentage
+
 
 st.header("SAS ML")
 
 with st.sidebar:
     st.title("Navigation")
-    option = st.radio("Select an option", ["Insert images", "Flood prediction", "Damage percentage","Areas analogy"])
+    option = st.radio("Select an option", ["Insert images", "Flood prediction", "Damage percentage", "Areas analogy"])
 
 if option == "Insert images":
     st.write("Insert images of the area")
-
-    label_name1 = st.text_input("Enter label name",key='1')
-    if label_name1 != "":
-        st.write("Label:", label_name1)
-        
-    label_name2 = st.text_input("Enter label name",key='2')
-    if label_name2 != "":
-        st.write("Label:", label_name2)
-
-    label_name3 = st.text_input("Enter label name",key='3')
-    if label_name3 != "":
-        st.write("Label:", label_name3)
-
-    label_name4 = st.text_input("Enter label name",key='4')
-    if label_name4 != "":
-        st.write("Label:", label_name4)
-    
-    label_name5 = st.text_input("Enter label name", key='5')
-    if label_name5 != "":
-        st.write("Label:", label_name5)
+    folder = 'TimewiseCSV'
+    file_list = os.listdir(folder)
+    selected_files = st.multiselect('Select file(s)', file_list)
+    if selected_files:
+        for filename in selected_files:
+            file_path = os.path.join(folder, filename)
+            with open(file_path, 'r') as file:
+                contents = file.read()
 
 if option == "Flood prediction":
-    st.write("Predicting whether a flood occurred or not")
-    data=pd.read_csv("flood_s1.csv")
-    if st.button("Predict"):
-        st.success(f"rain prediction")
-        fig = px.imshow(data.corr())
-        fig.update_layout(title="Correlation Heatmap")
-        fig.update_xaxes(title="Variables")
-        fig.update_yaxes(title="Variables")
-        st.plotly_chart(fig)
+    st.write("Predicting whether a flood occurred or not")    
+    folder = 'TimewiseCSV\\'
+    file_list = os.listdir(folder)
+    selected_files = st.multiselect('Select file(s)', file_list)
+    a=[]
+    if selected_files:
+        for filename in selected_files:
+            file_path = os.path.join(folder, filename)
+            data=set_data(file_path)
+            for images,labels in data:
+                for k in range(len(images)):
+                    try:
+                        p=model_prediction(images[k])
+                        a.append(p)
+                        if p==1:
+                            base="sen12flood\\sen12floods_s1_labels\\sen12floods_s1_labels\\"
+                            damage=damage_percentage()
+                    except:
+                        pass
 
+if st.button("Predict"):
+    st.success("rain prediction")
 if option == "Damage percentage":
     st.write("Computing damage percentage of an area")
-
+    
 if option == "Areas analogy":
     st.write("Comparing different regions")
+  
